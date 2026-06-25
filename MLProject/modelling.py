0b18c389045b3a6cac224681e00e1ed1d14d0dc7
SALMAN ASEGF <salmanfidinillah@usahidsolo.ac.id>
2026-06-25 10:54:09 +0700

    Remove tracking URI for GitHub Actions

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-# File ini untuk Kriteria Basic
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -12,11 +11,11 @@ y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 2. Setup MLflow & Autolog
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# BARIS set_tracking_uri DIHAPUS BIAR AMAN DI GITHUB ACTIONS
 mlflow.set_experiment("Membangun_Model_salmanfidinillah")
 mlflow.sklearn.autolog()
 
-# 3. Training (Autolog akan otomatis mencatat metrik, parameter, dan model)
+# 3. Training
 with mlflow.start_run(run_name="Basic_RandomForest"):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
