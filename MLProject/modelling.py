3a3baab8f7e692c8b051f64f1701a4b4f1747e7d
SALMAN ASEGF <salmanfidinillah@usahidsolo.ac.id>
2026-06-25 11:00:30 +0700

    Fix mlflow run ID conflict

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -11,12 +11,12 @@ y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 2. Setup MLflow & Autolog
-# BARIS set_tracking_uri DIHAPUS BIAR AMAN DI GITHUB ACTIONS
-mlflow.set_experiment("Membangun_Model_salmanfidinillah")
+# Kita TIDAK PAKAI set_experiment di sini biar nggak tabrakan sama command 'mlflow run'
 mlflow.sklearn.autolog()
 
 # 3. Training
-with mlflow.start_run(run_name="Basic_RandomForest"):
+# Parameter di dalam start_run() kita kosongin biar dia otomatis nerusin Run ID dari GitHub Actions
+with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
